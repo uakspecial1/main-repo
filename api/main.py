55bf02d736e87c07554c21501d63c82355e0b32a
uak <utkarshkandare123@gmail.com>
2024-10-09 17:12:00 +0530

    Updated

--- api/main.py
+++ api/main.py
@@ -384,7 +384,7 @@ async def search_similar_chunks(query: str):
         docsearch = LangChainPinecone.from_texts(data1, embeddings, index_name="pinecone")
 
     except Exception as e:
-        return[{"exception":e}]
+        return[{"exception":"hii"}]
 
     docs = docsearch.similarity_search(query)
     if docs:
